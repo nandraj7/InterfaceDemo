fccf5193391e9d41f37c33a206d0bd3559ab8848
hp <hp@HPELITEBOOK840G>
2023-04-25 17:55:35 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    internal class Employee : IEmployee
+    {
+        public void CalculateBonus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CalculateSalary()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
